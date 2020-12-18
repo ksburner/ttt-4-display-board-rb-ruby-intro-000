2c545ba2ac4372592544df79f6c08e701644e7f1
Kendra Swanson <kcs7@rice.edu>
2020-12-18 22:21:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,4 +3,7 @@
 def display_board(cells)
   puts " #{cells[0]} | #{cells[1]} |  #{cells[2]} "
   puts "-----------"
+  puts " #{cells[3]} | #{cells[4]} |  #{cells[5]} "
+  puts "-----------"
+  puts " #{cells[6]} | #{cells[7]} |  #{cells[8]} "
 end
