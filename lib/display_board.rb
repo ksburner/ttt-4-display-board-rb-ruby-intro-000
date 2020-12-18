5cc50b5dc3e9b2abbeaf2f6f0accd657d0eab9d1
Kendra Swanson <kcs7@rice.edu>
2020-12-18 22:21:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(cells)
+  puts " #{cells[0]} | #{cells[1]} |  #{cells[2]} "
+  puts "-----------"
 end
