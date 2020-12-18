7ea8e1e1a74f24664207c7e30ff92ea47eca109f
Kendra Swanson <kcs7@rice.edu>
2020-12-18 22:26:25 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -187,6 +187,8 @@ describe "#display_board in 'lib/display_board.rb" do
       #expect(true).to be(true)
 
       board = ["O", "O", "O", "O", "o", "O", "O", "O", "0"]
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | o | O")
