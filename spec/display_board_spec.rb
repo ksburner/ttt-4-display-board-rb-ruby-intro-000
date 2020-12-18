94dd499c9b18b6f59048c097d50622918199f708
Kendra Swanson <kcs7@rice.edu>
2020-12-18 22:24:40 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -184,7 +184,9 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
+
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
     end
   end
 end
