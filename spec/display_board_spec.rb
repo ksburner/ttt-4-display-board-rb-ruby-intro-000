110360bff07ea407463e3359aec5232056511835
Kendra Swanson <kcs7@rice.edu>
2020-12-18 22:26:40 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -191,7 +191,7 @@ describe "#display_board in 'lib/display_board.rb" do
       rows = output.split("\n")
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | o | O")
+      expect(rows[2]).to eq(" O | o | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | 0 ")
     end
